8034740f51e26914f76dcab8eab0ca8014f99520
Mabel <nasaadanna@gmail.com>
2026-04-21 09:01:26 +0100

    Add Render to ALLOWED_HOST

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5z24@iv+b40-n#d2nds=u0@v5ya5p212)tlp^0k3y&0r50j@fp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['noun-study-buddy-gcdgdec8ajgcbxhr.southafricanorth-01.azurewebsites.net', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['noun-study-buddy-gcdgdec8ajgcbxhr.southafricanorth-01.azurewebsites.net', '127.0.0.1', 'localhost', 'noun-study-buddy.onrender.com',]
 CSRF_TRUSTED_ORIGINS = ['https://noun-study-buddy-gcdgdec8ajgcbxhr.southafricanorth-01.azurewebsites.net']
 
 # Application definition
