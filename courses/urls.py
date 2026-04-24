2b9766ddae0ab6fbb287afd40bfc4df8a23d5feb
Mabel <nasaadanna@gmail.com>
2026-04-24 19:57:14 +0100

    Fix views issue

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+
 # Notice we added 'my_purchased_courses'  the imports below:
 from .views import (
     CourseViewSet, 
@@ -29,6 +30,6 @@ urlpatterns = [
     # This is the door for the APP to check "What courses do I own?"
     path('my-courses/', my_purchased_courses, name='my_purchased_courses'),
     # This is the door for the APP to check "What summaries do I have for this course?"
-    path('summaries/course/<int:course_id>/', views.get_summary_by_course, name='get_summary_by_course'),
+    path('summaries/course/<int:course_id>/', get_summary_by_course, name='get_summary_by_course'),
     # This lets Django serve PDF/Media files while we are in development mode!
 ]
